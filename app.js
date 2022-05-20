a4fa5edde1e182d0f3ed3af835ee37bdc2160463
Amos Bafuna <amosbafuna24@gmail.com>
2022-05-20 10:44:20 +0100

    Fix: timer counter

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 let i = 0;
+let time = 60;
 function move() {
   if (i == 0) {
     i = 1;
     let elem = document.querySelector(".Loading");
+    let timeLeft = document.querySelector(".time")
     let width = 100;
     let id = setInterval(frame, 1000);
     function frame() {
-      if (width <= 1) {
+      if (width < 1) {
         clearInterval(id);
         i = 0;
+        time = 60;
       } else {
-        width--;
+        width -= 1.66;
+        time--;
         elem.style.width = width + "%";
+        timeLeft.innerHTML = time;
       }
     }
   }
