9cb710e399658b3dfe6c7cdab1730a2381404e1a
Amos Bafuna <amosbafuna24@gmail.com>
2022-05-23 15:15:22 +0100

    feat: transition page

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+let errorName = document.querySelector(".errorName");
+let errorEmail = document.querySelector(".errorEmail");
+
+let form = document.querySelector("form");
+form.addEventListener("submit", function(e){
+  e.preventDefault()
+
+  let page1 = document.getElementById("page1");
+  let page2 = document.getElementById("page2");
+  let page3 = document.getElementById("page3");
+
+  let userName = document.querySelector(".input1").value;
+  let userEmail = document.querySelector(".input2").value;
+
+  if(!(userName !== '' && userEmail !== '')){
+    errorName.innerHTML = 'N’oubliez pas de renseigner votre nom avant de commencer le Quiz.';
+    errorEmail.innerHTML = 'N’oubliez pas de renseigner votre email avant de commencer le Quiz.';
+    
+  }
+  else{
+    page1.style.display = 'none';
+    page2.style.display = 'block';
+  }
+})
+
+// Timer management */
 let i = 0;
 let time = 60;
 function move() {
